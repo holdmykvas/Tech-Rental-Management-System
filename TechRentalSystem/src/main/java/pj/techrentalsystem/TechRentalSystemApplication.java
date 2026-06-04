92a669cfc76f465ba5a227bd4e5d561310687f0a
Nikita Pohrebniak <s32876@pjwstk.edu.pl>
2026-06-04 17:02:14 +0200

    Initial Commit

---
+++ TechRentalSystem/src/main/java/pj/techrentalsystem/TechRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package pj.techrentalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechRentalSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechRentalSystemApplication.class, args);
+    }
+
+}
---
+++ TechRentalSystem/src/test/java/pj/techrentalsystem/TechRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pj.techrentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechRentalSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
