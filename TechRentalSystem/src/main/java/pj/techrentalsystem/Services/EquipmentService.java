43f057a212394e279ed95d701cfc3d6769bcecb8
Nikita Pohrebniak <s32876@pjwstk.edu.pl>
2026-06-05 23:42:40 +0200

    Equipment manipulation logic added

--- TechRentalSystem/src/main/java/pj/techrentalsystem/Services/EquipmentService.java
+++ TechRentalSystem/src/main/java/pj/techrentalsystem/Services/EquipmentService.java
@@ -20,13 +20,37 @@ public class EquipmentService {
         return equipmentRepository.findAll();
     }
 
+    public Equipment getEquipmentById(Long id) {
+        return equipmentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Equipment with this id { " + id + " } is not found!"));
+    }
+
     public Equipment addEquipment(EquipmentDto equipmentDto) {
         Equipment equipment = new Equipment();
-        //TODO finish the validation
+        equipment.setName(equipmentDto.getName());
+        equipment.setDescription(equipmentDto.getDescription());
+        equipment.setPrice(equipmentDto.getPrice());
+        equipment.setStockQuantity(equipmentDto.getStockQuantity());
+        equipment.setCategory(equipmentDto.getCategory());
+
         return equipmentRepository.save(equipment);
     }
 
-    public Equipment getEquipmentById(Long id) {
-        return equipmentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Equipment with this id { " + id + " } is not found!"));
+    public Equipment updateEquipment(Long id,EquipmentDto equipmentDto){
+        Equipment equipment = equipmentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Equipment with id: " + id + " not found!"));
+
+        equipment.setName(equipmentDto.getName());
+        equipment.setDescription(equipmentDto.getDescription());
+        equipment.setPrice(equipmentDto.getPrice());
+        equipment.setStockQuantity(equipmentDto.getStockQuantity());
+        equipment.setCategory(equipmentDto.getCategory());
+
+        return equipmentRepository.save(equipment);
+    }
+
+    public void deleteEquipment(Long id) {
+        if (!equipmentRepository.existsById(id)) {
+            throw new IllegalArgumentException("Equipment with id: " + id + " not found!");
+        }
+        equipmentRepository.deleteById(id);
     }
 }
